3e942d4ea3be3b6907784753572c7f5b98419f27
redbeanbreadhasgit <69857133+redbeanbreadhasgit@users.noreply.github.com>
2022-05-27 10:01:41 +0800

    added first set of files for backend display

--- talentmatching/core/views.py
+++ talentmatching/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .models import MatchedJobModel, ApplicantModel, PredictionResultModel
+from .models import *
 
 
 # Create your views here.
