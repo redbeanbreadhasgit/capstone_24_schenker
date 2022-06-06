d08ffb526ffd169b469260e14670d24a9c6ec8ad
XuSog <1250021699@qq.com>
2022-06-06 21:57:54 +0800

    test

---
+++ capstone/src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './pages/sign-in/SignIn.js'
+import DashBoard from './pages/dashboard/Dashboard.js'
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import React from 'react';
+// //import ReactDOM from 'react-dom';
+// import Button from '@material-ui/core/Button';
+// // font
+// //import 'fontsource-roboto';
+
+// function App() {
+//   return (
+//     <p>
+//       <p color="primary">
+//       hi Capstone
+//       </p>
+//       <h1 color="primary">
+//       hi Capstone
+//       </h1>
+//       <h2 color="primary">
+//       hi Capstone
+//       </h2>
+
+//       <Button variant="contained" color="primary">
+//       hi Capstone
+//       </Button>
+//     </p>
+    
+//   );
+// }
+
+// // ReactDOM.render(<App />, document.querySelector('#app'));
+// export default App;
+
+function App() {
+  return (
+    <SignIn>
+    </SignIn>
+    // <DashBoard>
+    // </DashBoard>
+  );
+}
+
+export default App;
