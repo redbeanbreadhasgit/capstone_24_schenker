450331df969be25c1b9580775680ff2348136bee
shaoquan <57982242+assqueue@users.noreply.github.com>
2022-07-25 02:27:12 +0800

    ugly fix for sign up page
    Might need to figure out the template's form method.

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -6,6 +6,8 @@ Copyright (c) 2019 - present AppSeed.us
 # Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
 from .forms import LoginForm, SignUpForm
 
 
@@ -31,26 +33,38 @@ def login_view(request):
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
 
-def register_user(request):
-    msg = None
-    success = False
-
+##### SIGNUP ##############################################################################################################
+def register(request):
     if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            raw_password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=raw_password)
+        firstname = request.POST["firstname"]
+        lastname = request.POST["lastname"]
+        username = request.POST["username"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
 
-            msg = 'User created successfully.'
-            success = True
+        # if any of the inputs are blank
+        if firstname == "" or lastname == "" or username == "" or password == "" or password2 == "":
+            messages.info(request, "First Name / Last Name / Username / Password field cannot be blank")
+            return redirect('/')
 
-            # return redirect("/login/")
+        # if passwords are matching
+        elif password == password2:
 
+            # if username is taken
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "Username Taken")
+                return redirect('/')
+
+            # if all fields are correct
+            else:
+                user = User.objects.create_user(username=username, password=password, first_name=firstname, last_name=lastname)
+                user.save()
+                messages.info(request, "Account created, proceed to login")
+                return redirect('/')
+        
+        # passwords are not matching
         else:
-            msg = 'Form is not valid'
-    else:
-        form = SignUpForm()
-
-    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+            messages.info(request, "Password Not Matching")
+            return redirect('/')
+    else:   
+        return render(request, "accounts/register.html")
